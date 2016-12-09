0abec03f36d8c5e69ba048fadc5889911d8a8151
Iris <Iris@LAPTOP-GPLLOCK7>
2016-12-09 12:00:53 +0100

    something

--- designproject/src/designproject/Hello.java
+++ designproject/src/designproject/Hello.java
@@ -3,7 +3,7 @@ package designproject;
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("helloooooooooo");
+		System.out.println("helloooooooofdfdfdfoo");
 
 	}
 
