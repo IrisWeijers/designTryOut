5dca50387aa4735f05f03d22d8ecddbd8f6fccc9
IrisWeijers <i.weijers@student.utwente.nl>
2016-12-13 11:24:25 +0100

    commetn print hallo

--- designproject/src/designproject/Hello.java
+++ designproject/src/designproject/Hello.java
@@ -2,10 +2,12 @@ package designproject;
 
 public class Hello {
 	
-	public void hello(){
+	public Hello hello(){
+		return null;
 		
 	}
 
+	//print hallo
 	public static void main(String[] args) {
 		System.out.println("helloooooooooo");
 
